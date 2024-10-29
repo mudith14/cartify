8450cdd4a5292b671c2e6b045e22588df453f0ac
Mudith Reddy Gudipelly <mudithreddy.gudipelly@infor.com>
2024-10-29 19:43:22 +0530

    Initial commit

---
+++ src/main/java/com/example/ecom_proj/model/Product.java
@@ -0,0 +1,64 @@
+package com.example.ecom_proj.model;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String name;
+    private String description;
+    private String brand;
+    private BigDecimal price;
+    private String category;
+    private Date releaseDate;
+    private boolean productAvailable;
+    private int stockQuantity;
+    private String imageName;
+    private String imageType;
+    @Lob
+    private byte[] imageData;
+
+//    public void setImageName(String originalFilename) {
+//
+//    }
+//
+//    public void setImageType(String contentType) {
+//    }
+//
+//    public void setImageData(byte[] bytes) {
+//    }
+
+    public void setImageName(String imageName) {
+        this.imageName = imageName;
+    }
+
+    public void setImageType(String imageType) {
+        this.imageType = imageType;
+    }
+
+    public void setImageData(byte[] imageData) {
+        this.imageData = imageData;
+    }
+
+//    public byte[] getImageData() {
+//        return new byte[0];
+//    }
+//
+//    public String getImageType() {
+//        return "";
+//    }
+
+}
